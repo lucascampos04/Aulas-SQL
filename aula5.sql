bbf64d8b8a33607c07da9fc80f8354409f767d7e
lucascampos04 <camposdlucasoli@gmail.com>
2023-08-31 11:33:48 -0300

    Quinta aula

--- aula5.sql
+++ aula5.sql
@@ -1,81 +1,56 @@
--- Criação da tabela de autores
-CREATE TABLE autores(
-    id_autores INT PRIMARY KEY NOT NULL,
-    nome_autor VARCHAR(100)
+-- Create tables
+CREATE TABLE autor (
+  id INT PRIMARY KEY NOT NULL,
+  nome VARCHAR(200) NOT NULL
 );
 
--- Criação da tabela de livros
-CREATE TABLE livros(
-    id_livro INT PRIMARY KEY NOT NULL,
-    titulo VARCHAR(200),
-    ano_publi DATE NOT NULL,
-    quantidade_estoque INT NOT NULL
+CREATE TABLE livro (
+  id INT PRIMARY KEY NOT NULL,
+  titulo_livro VARCHAR(500) NOT NULL
 );
 
--- Criação da tabela de associação entre autores e livros (muitos-para-muitos)
-CREATE TABLE autores_livros(
-    id_livro INT NOT NULL,
-    id_autor INT NOT NULL,
-    PRIMARY KEY(id_livro, id_autor),
-    FOREIGN KEY(id_livro) REFERENCES livros(id_livro),
-    FOREIGN KEY(id_autor) REFERENCES autores(id_autores)
+CREATE TABLE livrosautores (
+  id_livro INT NOT NULL,
+  id_autor INT NOT NULL,
+  PRIMARY KEY (id_livro, id_autor),
+  FOREIGN KEY (id_livro) REFERENCES livro (id),
+  FOREIGN KEY (id_autor) REFERENCES autor (id)
 );
 
--- Criação da tabela de usuários
-CREATE TABLE usuarios(
-    id_usuarios INT PRIMARY KEY NOT NULL,
-    nome_usuario VARCHAR(100) NOT NULL,
-    email VARCHAR(400)
+CREATE TABLE usuario (
+  id INT PRIMARY KEY NOT NULL,
+  nome VARCHAR(200) NOT NULL
 );
 
--- Criação da tabela de empréstimos
-CREATE TABLE emprestimos(
-    id_emprestimo INT PRIMARY KEY NOT NULL,
-    id_usuario INT NOT NULL,
-    id_livros INT NOT NULL,
-    data_emprestimo DATE NOT NULL,
-    data_devolucao DATE NOT NULL,
-	FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuarios),
-	FOREIGN KEY(id_livros) REFERENCES livros(id_livro)
+CREATE TABLE emprestimo (
+  id INT PRIMARY KEY NOT NULL,
+  data_emp DATE NOT NULL,
+  id_usuario INT NOT NULL,
+  FOREIGN KEY (id_usuario) REFERENCES usuario (id)
 );
 
--- Inserção na tabela "autores"
-INSERT INTO autores (id_autores, nome_autor)
-VALUES (1, 'Autor 1');
+CREATE TABLE emprestimo_lista_livro (
+  id_livros INT NOT NULL,
+  id_emprestimo INT NOT NULL,
+  FOREIGN KEY (id_livros) REFERENCES livro (id),
+  FOREIGN KEY (id_emprestimo) REFERENCES emprestimo (id)
+);
 
--- Consulta na tabela "autores"
-SELECT * FROM autores;
+-- Insert data
+INSERT INTO autor (id, nome) VALUES (1, 'Leandor');
+INSERT INTO autor (id, nome) VALUES (2, 'Maria');
+INSERT INTO livro (id, titulo_livro) VALUES (1, 'Livro 1');
+INSERT INTO livro (id, titulo_livro) VALUES (2, 'Livro 2');
+INSERT INTO livro (id, titulo_livro) VALUES (3, 'Livro TESTE');
+INSERT INTO livrosautores (id_livro, id_autor) VALUES (1, 1);
+INSERT INTO livrosautores (id_livro, id_autor) VALUES (3, 1);
+INSERT INTO emprestimo (id, data_emp, id_usuario) VALUES (1, TO_DATE('31/07/2023', 'DD/MM/YYYY'), 1);
+INSERT INTO usuario (id, nome) VALUES (1, 'Lucas');
+INSERT INTO emprestimo_lista_livro (id_livros, id_emprestimo) VALUES (1, 1);
 
--- Inserção na tabela "livros"
-INSERT INTO livros (id_livro, titulo, ano_publi, quantidade_estoque)
-VALUES (1, 'Livro 1', to_date('29/06/2023', 'DD/MM/YYYY'), 10);
-
--- Consulta na tabela "livros"
-SELECT * FROM livros;
-
--- Inserção na tabela "autores_livros"
-INSERT INTO autores_livros (id_livro, id_autor)
-VALUES (1, 1);
-
--- Consulta na tabela "autores_livros"
-SELECT * FROM autores_livros;
-
--- Inserção na tabela "usuarios"
-INSERT INTO usuarios (id_usuarios, nome_usuario, email)
-VALUES (1, 'Usuário 1', 'usuario1@example.com');
-
--- Consulta na tabela "usuarios"
-SELECT * FROM usuarios;
-
--- Inserção na tabela "emprestimos"
-INSERT INTO emprestimos (id_emprestimo, id_usuario, id_livros, data_emprestimo, data_devolucao)
-VALUES (1, 1, 1, to_date('29/06/2023', 'DD/MM/YYYY'), to_date('29/06/2024', 'DD/MM/YYYY'));
-
--- Consulta na tabela "emprestimos"
-SELECT * FROM emprestimos;
-
-
--- tabela "autores" contém informações sobre os autores.
--- tabela "livros" contém informações sobre os livros.
--- tabela "autores_livros" armazena os relacionamentos entre autores e livros.
--- tabela "usuarios" guarda informações sobre os usuários da biblioteca.
+-- Select data
+SELECT * FROM autor;
+SELECT * FROM livro;
+SELECT * FROM livrosautores;
+SELECT * FROM emprestimo;
+SELECT * FROM emprestimo_lista_livro;
